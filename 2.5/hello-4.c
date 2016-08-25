2b9117a93adb571349a1a354102e8748fe41ee11
xihajuan2010@gmail.com <xihajuan2010@gmail.com>
2016-08-25 15:08:12 +0800

    add hello-4.c Makefile, Licensing and Module Documentation

---
+++ 2.5/hello-4.c
@@ -0,0 +1,25 @@
+#include<linux/module.h>
+#include<linux/kernel.h>
+#include<linux/init.h>
+#define DRIVER_AUTHOR "xxxx"
+#define DRIVER_DESC "XXXX"
+
+static int __init init_hello_4(void)
+{
+	printk(KERN_INFO "Hello, world 4\n");
+	return 0;
+}
+
+static void __exit cleanup_hello_4(void)
+{
+	printk(KERN_INFO "Goodbye, world 4\n");
+}
+
+module_init(init_hello_4);
+module_exit(cleanup_hello_4);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR(DRIVER_AUTHOR);
+MODULE_DESCRIPTION(DRIVER_DESC);
+MODULE_SUPPORTED_DEVICE("testdevice");
+
